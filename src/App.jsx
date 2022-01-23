c8d94fe796eee90a7604d1a32a3f87762f2cd011
Konton022 <42418105+Konton022@users.noreply.github.com>
2022-01-23 23:44:41 +0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Contacts from './pages/Contacts';
+import About from './pages/About';
+import Notfound from './pages/NotFound';
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Header />
+                <main className='container content'>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/contacts' element={<Contacts />} />
+                        <Route path='/about' element={<About />} />
+                        <Route element={<Notfound />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='navbar-fixed'>
+            <nav>
+                <div className='nav-wrapper'>
+                    <Link to='/' className='brand-logo'>
+                        Logo
+                    </Link>
+                    <ul className='right hide-on-med-and-down'>
+                        <li>
+                            <Link to='/contacts'>Contacts</Link>
+                        </li>
+                        <li>
+                            <Link to='/about'>About</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
